2e12206c5a18cabe2fea27be95642557e477d1ee
WhiteDazzle <93659023+WhiteDazzle@users.noreply.github.com>
2023-03-31 10:15:22 +0300

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <div className={styles.app}>
       <div className={styles['signIn-form-wrapper']}>
           <SignInForm/>
-         <a href='tel:89180721220' className='WelcomePack__link WelcomePack__link--number'> 89180721220 </a>
       </div>
     </div>
   );
