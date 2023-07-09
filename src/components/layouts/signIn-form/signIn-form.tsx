f9f87528e69f410918f3518e549276bf6b3725d7
Mihail Zadroga <mikhail.zadroga@balance-pl.ru>
2023-07-09 22:01:48 +0300

    добавлено отображение геолокации

--- src/components/layouts/signIn-form/signIn-form.tsx
+++ src/components/layouts/signIn-form/signIn-form.tsx
@@ -13,6 +13,7 @@ const SignInForm = () => {
   const [passwordValue, setPasswordValue] = useState("");
   const [emailValidationMessage, setEmailValidationMessage] = useState("");
   const [passwordValidationMessage, setPasswordValidationMessage] = useState("");
+  const [geo, setGeo] = useState<string[]>([])
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -21,7 +22,12 @@ const SignInForm = () => {
 
   function success(pos: any) {
     var crd = pos.coords;
-
+    setGeo([
+      'Ваше текущее местоположение:',
+      `Широта: ${crd.latitude}`,
+      `Долгота: ${crd.longitude}`,
+      `Плюс-минус ${crd.accuracy} метров.`
+    ])
     console.log('Ваше текущее местоположение:');
     console.log(`Широта: ${crd.latitude}`);
     console.log(`Долгота: ${crd.longitude}`);
@@ -34,48 +40,48 @@ const SignInForm = () => {
   return (
     <form className={styles["signIn-form"]}>
       <h1 className={styles.title}> welcome</h1>
-      <div className={styles["input-wrapper"]}>
-        <FormInput
-          value={emailValue}
-          onChange={({target:{value}})=> {
-              setEmailValue(value)
-              setEmailValidationMessage('')
-          }}
-          name="email"
-          type="email"
-          placeholder="введите email"
-          validationMessage={emailValidationMessage}
-          required
-          pattern={emailValidationPattern}
-        />
-      </div>
-      <div className={styles["input-wrapper"]}>
-        <FormInput
-          value={passwordValue}
-          onChange={({target:{value}})=> {
-              setPasswordValue(value)
-              setPasswordValidationMessage('')
-          }}
-          name="password"
-          type="password"
-          placeholder="введите пароль"
-          validationMessage={passwordValidationMessage}
-          required
-          minLength={6}
-          maxLength={40}
-        />
-      </div>
-      <input
-        type="submit"
-        value="Login"
-        className={styles["input-submit"]}
-        onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            setEmailValidationMessage(validateEmail(emailValue))
-            setPasswordValidationMessage(validatePassword(passwordValue))
-        }}
-      />
+      {/*<div className={styles["input-wrapper"]}>*/}
+      {/*  <FormInput*/}
+      {/*    value={emailValue}*/}
+      {/*    onChange={({target:{value}})=> {*/}
+      {/*        setEmailValue(value)*/}
+      {/*        setEmailValidationMessage('')*/}
+      {/*    }}*/}
+      {/*    name="email"*/}
+      {/*    type="email"*/}
+      {/*    placeholder="введите email"*/}
+      {/*    validationMessage={emailValidationMessage}*/}
+      {/*    required*/}
+      {/*    pattern={emailValidationPattern}*/}
+      {/*  />*/}
+      {/*</div>*/}
+      {/*<div className={styles["input-wrapper"]}>*/}
+      {/*  <FormInput*/}
+      {/*    value={passwordValue}*/}
+      {/*    onChange={({target:{value}})=> {*/}
+      {/*        setPasswordValue(value)*/}
+      {/*        setPasswordValidationMessage('')*/}
+      {/*    }}*/}
+      {/*    name="password"*/}
+      {/*    type="password"*/}
+      {/*    placeholder="введите пароль"*/}
+      {/*    validationMessage={passwordValidationMessage}*/}
+      {/*    required*/}
+      {/*    minLength={6}*/}
+      {/*    maxLength={40}*/}
+      {/*  />*/}
+      {/*</div>*/}
+      {/*<input*/}
+      {/*  type="submit"*/}
+      {/*  value="Login"*/}
+      {/*  className={styles["input-submit"]}*/}
+      {/*  onClick={(e) => {*/}
+      {/*      e.preventDefault()*/}
+      {/*      e.stopPropagation()*/}
+      {/*      setEmailValidationMessage(validateEmail(emailValue))*/}
+      {/*      setPasswordValidationMessage(validatePassword(passwordValue))*/}
+      {/*  }}*/}
+      {/*/>*/}
       <button
         type='button'
         className={styles["input-submit"]}
@@ -85,6 +91,7 @@ const SignInForm = () => {
       >
         получить геолокацию в консоли
       </button>
+      {geo.map((current: string)=> <p className={styles['geo-text']}>{current}</p>)}
     </form>
   );
 };
